17979970d049b2a2dd2abeb44c22534c2aca5b5f
yueweiwei <2422184607>
2021-06-29 19:17:01 +0800

    upd

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 #### 二维码用的的开源工具类 hutool里的QrCodeUtil
 #### 网址:http://hutool.mydoc.io/
+#### webSocket结合redis发布订阅
+
+#### 添加依赖
+
+     <groupId>com.google.zxing</groupId>
+      <artifactId>core</artifactId>
+     <version>3.3.3</version>
+     <groupId>cn.hutool</groupId>
+       <artifactId>hutool-all</artifactId>
+     <version>5.7.2</version>
+
---
+++ src/main/java/com/websocket/api/QrCodeApi.java
@@ -0,0 +1,47 @@
+package com.websocket.api;
+
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.extra.qrcode.QrCodeUtil;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Yue WeiWei
+ * @date 2021/6/29 17:04
+ */
+@CrossOrigin(origins = "*", allowCredentials = "true")
+@RestController
+@RequestMapping
+public class QrCodeApi {
+
+    /**
+     * 获取二维码
+     *
+     * @param request
+     * @param response
+     */
+    @GetMapping("getLoginQr")
+    public void createCodeImg(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+
+        try {
+            //这里没啥操作 就是生成一个UUID插入 数据库的表里。
+            //String uuid = userService.createQrImg();
+            String uuid = "445";
+            response.setHeader("uuid", uuid);
+            // 这里是开源工具类 huTool里的QrCodeUtil
+            QrCodeUtil.generate(uuid, 300, 300, "jpg", response.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
