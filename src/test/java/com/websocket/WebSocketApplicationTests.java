ae7ba15691218f6f901ba683a371a15839a90b93
yueweiwei <2422184607@qq.com>
2021-08-04 13:25:36 +0800

    Update WebSocketApplicationTests.java

--- src/test/java/com/websocket/WebSocketApplicationTests.java
+++ src/test/java/com/websocket/WebSocketApplicationTests.java
@@ -8,7 +8,7 @@ import redis.clients.jedis.Jedis;
 @SpringBootTest
 public class WebSocketApplicationTests {
     /**
-     * 测试和ServerEndpointExporter冲突
+     * 测试和ServerEndpointExporter冲突.
      * 去掉 @Bean
      */
 
