94b92e4917644e325b39b80bb12a076652c45a96
yueweiwei <2422184607@qq.com>
2021-08-05 15:37:42 +0800

    apend

--- src/test/java/com/websocket/WebSocketApplicationTests.java
+++ src/test/java/com/websocket/WebSocketApplicationTests.java
@@ -7,11 +7,11 @@ import redis.clients.jedis.Jedis;
 
 @SpringBootTest
 public class WebSocketApplicationTests {
+
     /**
      * 测试和ServerEndpointExporter冲突
      * 去掉 @Bean
      */
-
     @Test
     public void contextLoads() {
         System.out.println(RedisUtils.exists("6"));
